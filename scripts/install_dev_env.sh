8e9797f4da7a16cd9cea00cf5260cce611bcf7e3
Imran Ali <ImranA@coban.local>
2018-02-08 17:04:04 -0600

    latest development environment setup

--- scripts/install_dev_env.sh
+++ scripts/install_dev_env.sh
@@ -13,30 +13,56 @@ apt-get install -y python3-pip
 # virtualenvwrapper install for python3
 pip3 install virtualenvwrapper
 
-# make development folder that will be visible in windows explorer
-mkdir /home/vagrant/development
-chown vagrant /home/vagrant/development
-chgrp vagrant /home/vagrant/development
+# # make development folder that will allow host OS to host projects natively
+mkdir /vagrant/development
+# chmod 777 /home/vagrant/development
+# chown vagrant /home/vagrant/development
+# chgrp vagrant /home/vagrant/development
 
-# configure samba
-echo "[dev]" >> /etc/samba/smb.conf
-echo "path=/home/vagrant/development" >> /etc/samba/smb.conf
-echo "read only = no" >> /etc/samba/smb.conf
-echo "writeable = yes" >> /etc/samba/smb.conf
-echo "browseable = yes" >> /etc/samba/smb.conf
-echo "guest ok = yes" >> /etc/samba/smb.conf
-echo "create mask = 0755" >> /etc/samba/smb.conf
-echo "directory mask = 0755" >> /etc/samba/smb.conf
+# make a remote virtual env folder to allow guest OS to create run environments natively
+mkdir /home/vagrant/~envs
+chown vagrant /home/vagrant/~envs
+chgrp vagrant /home/vagrant/~envs
+chmod 777 /home/vagrant/~envs
+# chmod 777 /home/vagrant/~envs
+# chown vagrant /home/vagrant/~envs
+# chgrp vagrant /home/vagrant/~envs
+
+# # configure samba
+# ## create development folder access to windows
+# echo "[dev]" >> /etc/samba/smb.conf
+# echo -e "\tpath = /home/vagrant/development" >> /etc/samba/smb.conf
+# echo -e "\tread only = no" >> /etc/samba/smb.conf
+# echo -e "\twriteable = yes" >> /etc/samba/smb.conf
+# echo -e "\tpublic = yes" >> /etc/samba/smb.conf
+# echo -e "\tbrowseable = yes" >> /etc/samba/smb.conf
+# echo -e "\tguest ok = yes" >> /etc/samba/smb.conf
+# echo -e "\tcreate mask = 0755" >> /etc/samba/smb.conf
+# echo -e "\tdirectory mask = 0755" >> /etc/samba/smb.conf
+
+# ## create home/vagrant folder access to windows 
+echo "[venv]" >> /etc/samba/smb.conf
+echo -e "\tpath = /home/vagrant/~envs" >> /etc/samba/smb.conf
+echo -e "\tread only = no" >> /etc/samba/smb.conf
+echo -e "\twriteable = yes" >> /etc/samba/smb.conf
+echo -e "\tpublic = yes" >> /etc/samba/smb.conf
+echo -e "\tbrowseable = yes" >> /etc/samba/smb.conf
+echo -e "\tguest ok = yes" >> /etc/samba/smb.conf
+echo -e "\tcreate mask = 0777" >> /etc/samba/smb.conf
+echo -e "\tdirectory mask = 0777" >> /etc/samba/smb.conf
+
+service smbd restart
 
 # configure virtual env for python3 by adding these lines to .profile
 echo "export WORKON_HOME=/home/vagrant/~envs" >> /home/vagrant/.profile
-echo "export PROJECT_HOME=/home/vagrant/development" >> /home/vagrant/.profile
+echo "export PROJECT_HOME=/vagrant/development" >> /home/vagrant/.profile
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> /home/vagrant/.profile
 echo "export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv" >> /home/vagrant/.profile
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.profile
 
 # run .profile
 source /home/vagrant/.profile
+
 
 
 
